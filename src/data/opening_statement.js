3df2ef21d437e559f7a800c110b7bb4dcd57fafa
Austin <austin.w.chappell@gmail.com>
2020-02-28 18:52:58 -0600

    remove photos

--- src/data/opening_statement.js
+++ src/data/opening_statement.js
@@ -1,7 +1,3 @@
-import CavazosPhoto from '../images/quotes/david-cavazos.jpg';
-import WestPhoto from '../images/quotes/matthew-west.jpg';
-import SatherPhoto from '../images/quotes/cameron-sather.jpg';
-
 export const OPENING_STATEMENT_INTRO = [
   'Opening Statement is a collection of beginner to intermediate snare drum solos. For the past 20 years, I have found a need for beginning to intermediate snare literature with greater variety. Something that included open and closed rolls, different rhythms, dynamics, stickings, flams and yet was accessible and easy to read for beginning snare drummers. Opening Statement represents four years of composing, testing and receiving critical feedback from several Texas High School percussion programs ranging in size and ability level.',
 ];
@@ -9,19 +5,16 @@ export const OPENING_STATEMENT_INTRO = [
 export const OPENING_STATEMENT_QUOTES = [
   {
     name: 'Matt Ehlers',
-    avatar: SatherPhoto,
     titles: ['Associate Director of Bands/Director of Percussion', 'McCallum High School Fine Arts Academy', 'Austin, TX'],
     quote: 'Doug Bush has done it again! "Opening Statement" fills a massive void in beginner-level snare drum solo literature. Doug has done such an amazing job writing these solos in a manner that is educational yet still fresh and fun. This book will lead your students straight into "Super Hands" and will set up the solid foundation needed for "Master Hands." It is a must-have for every band program.',
   },
   {
     name: 'Jimmy LaBrecque',
-    avatar: WestPhoto,
     titles: ['Active Clinician and former Percussion Director', 'Highland Park and Lake Highlands High Schools'],
     quote: 'Doug Bush\'s new book "Opening Statement" is fantastic! An absolute must-add to every percussion curriculum. Doug\'s solos include a huge variety of modern rudimental skills, contemporary rhythmic and spatial demands, and an extremely wide range of difficulty levels. There is a solo for EVERY student in this book. Doug follows up his masterwork, "Super Hands," with another essential Snare Drum book! Thank You Mr. Bush!',
   },
   {
     name: 'Brian Teed',
-    avatar: CavazosPhoto,
     titles: ['Director of Percussion Studies', 'Wakeland High School', 'Frisco, TX'],
     quote: 'Doug Bush has created the perfect compilation of snare drum solos that are achievable across all ages and musical ability levels. Each etude helps to develop student\'s knowledge and understanding of modern-day rhythms, dynamics, and timing, while also being enjoyable to learn and perform. Bush\'s writing in "Opening Statement" compliments his "Super Hands" method book and paves the way to his "Master Hands" method book, making it a no-brainer for all percussion programs to add to their curriculum.',
   },
@@ -29,7 +22,7 @@ export const OPENING_STATEMENT_QUOTES = [
 
 export const OPENING_STATEMENT_VIDS = [
   {
-    description: 'Opening Statement is a collection of beginner to intermediate snare drum solos. For the past 20 years, I have found a need for beginning to intermediate snare literature with greater variety. Something that included open and closed rolls, different rhythms, dynamics, stickings, flams and yet was accessible and easy to read for beginning snare drummers. Opening Statement represents four years of composing, testing and receiving critical feedback from several Texas High School percussion programs ranging in size and ability level.',
+    description: 'Opening Statement Promo',
     title: 'Opening Statement',
     youtubeId: 'RBeMHIFD_bs',
   },
