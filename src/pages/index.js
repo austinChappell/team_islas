61eea16eb965d3267414371bc55f1e8849207caa
Austin Chappell <austin.w.chappell@gmail.com>
2020-08-28 05:36:20 -0500

    add flex grow to Layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { css } from 'react-emotion';
-import YouTube from 'react-youtube';
 
 import Layout from '../components/layout';
 
@@ -9,10 +8,8 @@ import {
   Heading1,
 } from '../components/shared';
 
-const MODAL_PADDING = 10;
 
 import HomeBg from '../images/home-bg.jpg';
-import { COLORS } from '../constants';
 
 const homeLayout = css({
   backgroundImage: `url(${HomeBg})`,
@@ -20,73 +17,23 @@ const homeLayout = css({
   textAlign: 'center',
 });
 
-class IndexPage extends Component {
-  modalWrapper = React.createRef();
+const IndexPage = () => {
+  console.log('index page');
 
-  state = {
-    videoOptions: {
-      height: null,
-      playerVars: {}, // https://developers.google.com/youtube/player_parameters
-      width: null,
-      marginBottom: 0,
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', this.setVideoDimensions);
-    this.setVideoDimensions();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.setVideoDimensions);
-  }
-
-
-  setVideoDimensions = () => {
-    const videoContainerWidth = this.modalWrapper.clientWidth;
-    const { videoOptions } = this.state;
-    const aspectRatio = 9 / 16;
-    videoOptions.height = `${(videoContainerWidth * aspectRatio) - MODAL_PADDING * 2}px`;
-    videoOptions.width = `${videoContainerWidth - MODAL_PADDING * 2}px`;
-    this.setState({ videoOptions });
-  }
-
-  render() {
-    const { videoOptions } = this.state;
-    const videoStyle = css`
-      margin-bottom: 0;
-      height: ${videoOptions.height};
-      width: ${videoOptions.width};
-    `;
-
-    return (
-      <Layout
-        className={homeLayout}
-        overlayColor="rgba(0, 0, 0, 0.5)"
-      >
+  return (
+    <Layout
+      className={homeLayout}
+      overlayColor="rgba(0, 0, 0, 0.5)"
+    >
       <Container>
-        <div style={{ margin: 15 }}>
+        <div style={{ margin: 50 }}>
           <Heading1 light>
             New Album Coming Soon!
           </Heading1>
         </div>
-        <div
-          ref={el => this.modalWrapper = el}
-          style={{
-            backgroundColor: 'white',
-            padding: '10px',
-            margin: '0 auto',
-          }}
-        >
-          <YouTube
-            className={videoStyle}
-            videoId="Lnul3DwS_bc"
-          />
-        </div>  
       </Container>
     </Layout>
-      )
-  }
-}
+  );
+};
 
 export default IndexPage;
